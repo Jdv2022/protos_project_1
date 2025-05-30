0426363578e0475ed01e02d3456b7a079e6951fe
jhon <jhondennivistal@gmail.com>
2025-05-30 21:40:07 +0800

    UP

--- protos_client/ClientService.php
+++ protos_client/ClientService.php
@@ -5,19 +5,29 @@ namespace protos_project_1\protos_client;
 use grpc\GetUserDetails\GetUserDetailsServiceClient;
 use grpc\Register\RegisterServiceClient;
 use grpc\userRegistrationFormData\UserRegistrationFormDataServiceClient;
+use grpc\getUsers\GetUsersServiceClient;
+use grpc\userClockIn\UserClockInService;
 
 class ClientService {
-
+	// UMS
     public function getUserServiceClient(): GetUserDetailsServiceClient {
 		return $this->userManagementServiceClient(GetUserDetailsServiceClient::class);
 	}
-	
+	// UMS
 	public function getRegisterServiceClient(): RegisterServiceClient {
 		return $this->userManagementServiceClient(RegisterServiceClient::class);
 	}
-
+	// UMS
 	public function getRegistrationFormDataClient(): UserRegistrationFormDataServiceClient {
 		return $this->userManagementServiceClient(UserRegistrationFormDataServiceClient::class);
+	}
+	// UMS
+	public function getUsers(): GetUsersServiceClient {
+		return $this->userManagementServiceClient(GetUsersServiceClient::class);
+	}
+	// UMS
+	public function clockIn(): UserClockInService {
+		return $this->userManagementServiceClient(UserClockInService::class);
 	}
 
 	protected function userManagementServiceClient(string $class): mixed {
